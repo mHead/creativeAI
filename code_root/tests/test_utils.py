9e90b2db29665c0abb4d4ffb90765ef626e34023
Marco Testa <marco_testa@icloud.com>
2021-06-04 01:53:55 +0200

    I think it will work now, for sure

--- code_root/tests/test_utils.py
+++ code_root/tests/test_utils.py
@@ -3,9 +3,9 @@ from ..musicSide.DatasetMusic2emotion.tools.utils import ms2samples, samples2ms
 
 # dumb test
 def test_samples2ms():
-    a = samples2ms(22050)
+    a = samples2ms(samples=22050)
     assert a == 500
 
 def test_ms2samples():
-    a = ms2samples(500)
+    a = ms2samples(sample_rate=500)
     assert a == 22050
